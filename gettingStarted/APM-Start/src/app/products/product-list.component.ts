466880535e7843e936ea9d52888c36a10cf5aec6
gloria-h <gloria@gloria-ho.com>
2019-10-30 16:45:57 -0400

    added stars to rating column, fixed issue with @include font-awesome in style.css by using CDN, star rating not pulling the correct rating number, star rating not setting the correct width, star component listening for ngOnChanges() which never triggers

--- gettingStarted/APM-Start/src/app/products/product-list.component.ts
+++ gettingStarted/APM-Start/src/app/products/product-list.component.ts
@@ -14,7 +14,6 @@ export class ProductListComponent implements OnInit{
     imageMargin: number = 2;
     showImage: boolean = false;
     
-    // listFilter: string= 'cart';
     _listFilter: string;
     get listFilter(): string {
         return this._listFilter;
