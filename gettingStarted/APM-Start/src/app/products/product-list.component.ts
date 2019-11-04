4f83d3810e8c300cd86fd5875a33a74c4a0a4460
gloria-h <gloria@gloria-ho.com>
2019-11-04 11:22:00 -0500

    star click listener functional, title update functional

--- gettingStarted/APM-Start/src/app/products/product-list.component.ts
+++ gettingStarted/APM-Start/src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -28,52 +29,10 @@ export class ProductListComponent implements OnInit{
     }
     
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        },
-        {
-            "productId": 8,
-            "productName": "Saw",
-            "productCode": "TBX-0022",
-            "releaseDate": "May 15, 2019",
-            "description": "15-inch steel blade hand saw",
-            "price": 11.55,
-            "starRating": 3.7,
-            "imageUrl": "assets/images/saw.png"
-        }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = '';
+    constructor(private productService: ProductService) {
+        // this.listFilter = 'cart'; // default search params
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -87,7 +46,7 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        // throw new Error("Method not implemented.");
-        console.log("In OnInit");
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
     }
 } 
